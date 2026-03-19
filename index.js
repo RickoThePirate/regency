054a2081f31dd2d37fe875e30ea53d945a55120a
RickoThePirate <rickoprivat@gmail.com>
2026-03-19 18:20:30 +0100

    index.js

--- index.js
+++ index.js
@@ -1,42 +1,77 @@
+require("dotenv").config();
+
 const express = require("express");
 const axios = require("axios");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-const CLIENT_ID = "1448300104376389682";
-const CLIENT_SECRET = "l0NYrGdY_FyA_CqxJs1ij0POgS15knpI";
-const REDIRECT_URI = "https://charismatic-inspiration-production.up.railway.app/callback";
+// Hent fra .env (VIGTIGT!)
+const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
+const REDIRECT_URI = process.env.REDIRECT_URI;
+const FRONTEND_URL = process.env.FRONTEND_URL;
 
+// Root (kun test)
 app.get("/", (req, res) => {
-  res.send('<a href="/login">Login med Discord</a>');
+  res.send("Backend kører ✅");
 });
 
+// Login route
 app.get("/login", (req, res) => {
-  const url = `https://discord.com/api/oauth2/authorize?client_id=${CLIENT_ID}&response_type=code&redirect_uri=${REDIRECT_URI}&scope=identify guilds`;
+  const url = `https://discord.com/api/oauth2/authorize?client_id=${CLIENT_ID}&response_type=code&redirect_uri=${encodeURIComponent(REDIRECT_URI)}&scope=identify`;
   res.redirect(url);
 });
 
+// Callback fra Discord
 app.get("/callback", async (req, res) => {
   const code = req.query.code;
 
-  const token = await axios.post("https://discord.com/api/oauth2/token",
-    new URLSearchParams({
-      client_id: CLIENT_ID,
-      client_secret: CLIENT_SECRET,
-      grant_type: "authorization_code",
-      code: code,
-      redirect_uri: REDIRECT_URI
-    }),
-    { headers: { "Content-Type": "application/x-www-form-urlencoded" } }
-  );
+  if (!code) {
+    return res.redirect(FRONTEND_URL);
+  }
 
-  const user = await axios.get("https://discord.com/api/users/@me", {
-    headers: {
-      Authorization: `Bearer ${token.data.access_token}`
-    }
-  });
+  try {
+    // Hent token
+    const tokenRes = await axios.post(
+      "https://discord.com/api/oauth2/token",
+      new URLSearchParams({
+        client_id: CLIENT_ID,
+        client_secret: CLIENT_SECRET,
+        grant_type: "authorization_code",
+        code: code,
+        redirect_uri: REDIRECT_URI
+      }),
+      {
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded"
+        }
+      }
+    );
 
-  res.send(`Du er logget ind som: ${user.data.username}`);
+    const access_token = tokenRes.data.access_token;
+
+    // Hent bruger info
+    const userRes = await axios.get(
+      "https://discord.com/api/users/@me",
+      {
+        headers: {
+          Authorization: `Bearer ${access_token}`
+        }
+      }
+    );
+
+    const user = userRes.data;
+
+    // Send tilbage til din hjemmeside
+    res.redirect(`${FRONTEND_URL}?username=${user.username}&avatar=${user.id}/${user.avatar}`);
+
+  } catch (err) {
+    console.error(err.response?.data || err.message);
+    res.redirect(FRONTEND_URL);
+  }
 });
 
-app.listen(3000, () => console.log("Server kører"));
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server kører på port ${PORT}`);
+});
