06ca61df1e67e89913982e48f88391a9ad187e78
RickoThePirate <rickoprivat@gmail.com>
2026-03-19 17:42:18 +0100

    index.js

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const axios = require("axios");
 const app = express();
+const PORT = process.env.PORT || 8080;
 
 const CLIENT_ID = "1448300104376389682";
 const CLIENT_SECRET = "l0NYrGdY_FyA_CqxJs1ij0POgS15knpI";
 const REDIRECT_URI = "https://charismatic-inspiration-production.up.railway.app/callback";
-
+Deploy
 app.get("/", (req, res) => {
   res.send('<a href="/login">Login med Discord</a>');
 });
